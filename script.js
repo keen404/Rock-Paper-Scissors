4448c1657ce1b38c484bb56280e2aac861239a20
Keen <2003keen@gmail.com>
2025-03-28 09:08:31 +0700

    Rename variable in getComputerChoice

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ function getComputerChoice() {
     // Create variable name x to store random number
     let x = 0;
     // Create variable name result to store a value of "Rock", "Paper", "Scissors"
-    let result;
+    let computerChoice;
     // Random number in range of 3 assign to x
     x = Math.floor(Math.random() * 3)
     // IF x is equal 0
     if (x === 0) {
         // THEN assign "Rock" to result 
-        result = "Rock"
+        computerChoice = "Rock"
     }
     //  ELSE IF result is equal 1
     else if (x === 1) {
         // THEN assign "Paper" to result
-        result = "Paper"
+        computerChoice = "Paper"
     }
     // ELSE
     else {
         // Assign Scissors  to result
-        result = "Scissors"
+        computerChoice = "Scissors"
     }
     // END IF
     // Return result
-    return result
+    return computerChoice
 }
 
+function getHumanChoice() {
+    // Create a variable to store user choice
+    let UserChoice;
+    // Ask user for choice
+    // Return user choice
+
+}
